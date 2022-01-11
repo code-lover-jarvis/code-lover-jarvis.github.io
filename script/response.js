c9ba4e8ed63e25f65f564cca013af7cce7d6ef33
code-lover-jarvis <jibinjoyjuly1996@gmail.com>
2022-01-11 18:28:12 +0530

    responsive edits

---
+++ script/response.js
@@ -0,0 +1,9 @@
+function toggling(){
+  document.getElementById('mb').className = 'hide';
+  document.getElementById('main').className = 'main_unhide'
+}
+function toggling2()
+{
+  document.getElementById('mb').className = 'menu_button';
+  document.getElementById('main').className = 'main'
+}
